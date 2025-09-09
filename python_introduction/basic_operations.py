8b1da2f10770cbdf28f2ae0492376ab9373b098b
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-09-09 16:20:29 +0100

    This is my python codes to the arithmetric

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,22 @@
+# Variables for arithmetric
+number1 = 5
+number2 = 10
+
+# The Addition of arithmetric
+sum = number1 + number2
+
+print(sum)
+
+# The Difference of arithmetric
+difference = number2 - number1
+
+print(difference)
+
+# The Product of arithmetric
+product = number1 * number2
+
+print(product)
+
+
+
+
