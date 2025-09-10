593e0cab15a91eeb5c58824f3814a2a3874a203a
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-09-10 08:13:41 +0100

    simple interest python code.

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+#assigning values for P,R,T
+
+principal = 1000
+rate = 0.05
+time = 3
+ 
+#calculation of interest
+interest = principal * rate * time
+
+print("The simple interest is:", interest)
