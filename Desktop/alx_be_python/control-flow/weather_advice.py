031cc6f5ce229e67ac074a3591b1146794691aa8
Paul Emmanuel Chukwuma <chukspaul777@gmail.com>
2025-09-19 17:24:55 +0100

    Get weather advice

---
+++ Desktop/alx_be_python/control-flow/weather_advice.py
@@ -0,0 +1,10 @@
+weather_today = input("What is the weather ike today ? (sunny/rainy/cold:)")
+
+if weather_today == "sunny":
+ print("Wear a t-shirt and sunglasses")
+elif weather_today == "rainy":
+ print("Don,t forget your umbrella and a raincoat")
+elif weather_today == "cold":
+ print("Make sure to wear a warm coat and a scarf")
+else: 
+ print("Sorry, I don't have recommendations for this weather")
